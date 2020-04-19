97a0f4e64790e2fb916588c47593b462e5228cf7
Colm Sheehan <colms152@gmail.com>
2020-04-19 12:38:59 +0100

    fix

--- server.js
+++ server.js
@@ -24,6 +24,25 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/whoami', function(req, res) {
+  var ip = req.headers['x-forwarded-for'] || 
+  req.connection.remoteAddress || 
+  req.socket.remoteAddress ||
+  req.connection.socket.remoteAddress;
+  var info = {
+    'ipaddress': ip,
+    'language': req.headers["accept-language"].split(',')[0],
+    'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+  };
+  res.send(info);
+ });
 
 
 // listen for requests :)
